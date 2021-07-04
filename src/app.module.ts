0df565ae8ebf267f0ee88766fcd11a201d54bc34
argon1025 <argon1025@gmail.com>
2021-07-04 19:07:39 +0900

    Add /notes Endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenModule } from './token/token.module';
 import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [TokenModule, UsersModule],
+  imports: [TokenModule, UsersModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
