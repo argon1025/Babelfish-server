c136c7726486349049dacfc5c96f2db611977a2d
argon1025 <argon1025@gmail.com>
2021-07-04 19:06:58 +0900

    Add /users Endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenModule } from './token/token.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TokenModule],
+  imports: [TokenModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
