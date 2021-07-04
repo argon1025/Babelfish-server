6fad89ee1c449af51aa774d06257a434b38e8780
argon1025 <argon1025@gmail.com>
2021-07-04 18:34:55 +0900

    Add configModule #2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,13 +7,21 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configuration Load
+  const configService = app.get<ConfigService>(ConfigService);
+  const SERVER_PORT = configService.get<number>('SERVER_PORT', 8080);
+  const SERVER_ENV = configService.get<string>('NODE_ENV', 'production');
+  const SERVER_HOST = configService.get<string>('SERVER_HOST', 'localhost');
+
+  await app.listen(SERVER_PORT);
 
   // Hot-Module
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-  console.log('Server started!!');
+
+  console.log(`${SERVER_ENV} server http://${SERVER_HOST}:${SERVER_PORT}`);
 }
 bootstrap();
