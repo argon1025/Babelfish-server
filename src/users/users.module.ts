8fe0013ae292e785b862414df8e27aeeff0417a2
argon1025 <argon1025@gmail.com>
2021-07-08 18:55:11 +0900

    Add service userAlreadyExist

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from '../entities/Member';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Member])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Member } from '../entities/Member';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(Member) private membersRepository: Repository<Member>,
+  ) {}
+
+  async userAlreadyExist(userId: string): Promise<boolean> {
+    const userData = await this.membersRepository.findOne({ userid: userId });
+    console.log(userData);
+    if (userData) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
