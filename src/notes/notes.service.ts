2f513d9d2629752d0f8ff9bbab4ce4d9ba234b2e
argon1025 <argon1025@gmail.com>
2021-07-14 22:12:48 +0900

    Add #16 NoteService CRUD

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -7,17 +7,36 @@ import { Note } from '../entities/Note';
 export class NotesService {
   constructor(@InjectRepository(Note) private noteRepository: Repository<Note>) {}
   // 유저 노트 리스트를 조회해 반환
-  getUserNoteList() {}
+  async getUserNoteList(userId: string): Promise<Note[]> {
+    const userNoteListData = await this.noteRepository.find({ where: { memberEmail: userId } });
+    console.log(userNoteListData);
+    return userNoteListData;
+  }
 
   // 유저 노트를 생성
-  createUserNote() {}
+  async createUserNote(userId: string, noteName: string) {
+    const createNoteResult = await this.noteRepository.save({ memberEmail: userId, notename: noteName, learningDay: null });
+    console.log(createNoteResult);
+  }
 
   // 유저 노트 정보 수정
-  modifyUserNote() {}
+  async modifyUserNote(noteId: number, userId: string, noteName: string) {
+    const modifyNoteResult = await this.noteRepository.update({ id: noteId, memberEmail: userId }, { notename: noteName });
+    console.log(modifyNoteResult);
+  }
 
   // 유저 노트 삭제
-  deleteUserNote() {}
+  async deleteUserNote(noteId: number, userId: string) {
+    const deleteNoteResult = await this.noteRepository.delete({ id: noteId, memberEmail: userId });
+    console.log(deleteNoteResult);
+  }
 
   // 유저 노트 학습상태 갱신
-  learningStatusUpdateForUserNote() {}
+  async learningStatusUpdateForUserNote(noteId: number, userId: string) {
+    const NOW_DATE = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const noteDataResult = await this.noteRepository.findOne({ id: noteId, memberEmail: userId });
+    console.log(noteDataResult);
+    const updateNoteResult = await this.noteRepository.update({ id: noteId, memberEmail: userId }, { learningDay: NOW_DATE, learningCount: 2 });
+    console.log(updateNoteResult);
+  }
 }
