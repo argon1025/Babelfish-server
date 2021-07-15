c6e57cd838f992451cea95b8a531909b35542b39
argon1025 <argon1025@gmail.com>
2021-07-15 20:24:16 +0900

    Add #18 Word Serivce functions

--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Note } from '../entities/Note';
 
 @Injectable()
-export class WordsService {}
+export class WordsService {
+  constructor(@InjectRepository(Note) private notesRepository: Repository<Note>) {}
+  async getUserWordList(noteId: number, userId: string) {}
+  async createUserWord(noteId: number, userId: string, wordTitle: string, wordMean1: string, wordMean2: string) {}
+  async modifyUserWord(noteId: number, wordId: number, userId: string, wordTitle: string, wordMean1: string, wordMean2: string) {}
+  async deleteUserWord(noteId: number, wordId: number, userId: string) {}
+  async addWrongCountToWord(noteId: number, wordId: number, userId: string) {}
+}
