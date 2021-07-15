8e439e98989cb0d5ef192a5fb399b8dd5c686fde
argon1025 <argon1025@gmail.com>
2021-07-15 20:14:19 +0900

    Add #18 Import TypeORM module

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WordsService } from './words.service';
 import { WordsController } from './words.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from '../entities/Note';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Note])],
   controllers: [WordsController],
-  providers: [WordsService]
+  providers: [WordsService],
 })
 export class WordsModule {}
