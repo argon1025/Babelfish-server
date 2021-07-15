c6896b26adafb70bdb07f4868f11fbd9f6b719a8
argon1025 <argon1025@gmail.com>
2021-07-15 20:54:19 +0900

    Fix #18 word TypeORM module import error

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { WordsService } from './words.service';
 import { WordsController } from './words.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Note } from '../entities/Note';
+import { Word } from '../entities/Word';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note])],
+  imports: [TypeOrmModule.forFeature([Word])],
   controllers: [WordsController],
   providers: [WordsService],
 })
