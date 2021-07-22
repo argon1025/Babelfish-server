67156af9ece816b65f42b94a941aa6cf06f39cc6
argon1025 <argon1025@gmail.com>
2021-07-22 18:10:45 +0900

    Add #20 RoleGuard

---
+++ src/common/Guard/Role.Guard.ts
@@ -0,0 +1,17 @@
+import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '../Service/JWT.Service';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    // 토큰 권한 검증
+    if (this.jwtService.roleVerify(request.headers.token, request.params.useremail)) {
+      return true;
+    } else {
+      throw new HttpException({ msg_code: '4', msg: '토큰데이터가 올바르지 않습니다' }, 401);
+    }
+  }
+}
